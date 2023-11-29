b179ac311f52dad5906b11720fce70361d3f2b8e
OOOOnlyyou <84295251+OOOOnlyyou@users.noreply.github.com>
2023-11-29 14:49:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from auto_get_terminal_info_zto import GetModelParameter
 
+# -----------------------------Sample-----------------------------------------------
 s = time.time()
 gmp = GetModelParameter(savePath="./phone_info_zto_top.csv", mode=0, browserDriverPath='./chromedriver')
 gmp.main(_startPage=3, _endPage=5)
